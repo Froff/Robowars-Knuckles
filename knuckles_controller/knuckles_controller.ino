e3ccb725ee842eb85c058e57c27df464f34a2da2
Froff <froff@protonmail.com>
2018-03-01 22:25:16 +0100

    Radio is working, pinging out and back

--- knuckles_controller/knuckles_controller.ino
+++ knuckles_controller/knuckles_controller.ino
@@ -5,16 +5,19 @@
 #include <LiquidCrystal.h>
 
 // Pins ************************************************************************
-#define BUTTON_PIN 8
+#define BUTTON_PIN A5
 #define X_AXIS_PIN A7
 #define Y_AXIS_PIN A6
 #define LED_PIN 2
-#define LCDRS 7
-#define LCDEN 6
-#define LCDD4 5
-#define LCDD5 4
-#define LCDD6 3
-#define LCDD7 2
+#define LCDRS 8
+#define LCDEN 7
+#define LCDD4 6
+#define LCDD5 5
+#define LCDD6 4
+#define LCDD7 3
+#define RADIOCE 9
+#define RADIOCS 10
+#define RADIO_INTERRUPT_PIN 2
 
 // Constants *******************************************************************
 #define CALIBRATION_TIME_MILLIS 2000 // Time for calibrating inputs (like joystick) at the start
@@ -75,10 +78,10 @@ private:
 
 // Globals *********************************************************************
 Joystick joy (X_AXIS_PIN, Y_AXIS_PIN, BUTTON_PIN);
-RF24 radio(9,10);
+RF24 radio(RADIOCE, RADIOCS);
 LiquidCrystal lcd (LCDRS, LCDEN, LCDD4, LCDD5, LCDD6, LCDD7);
 int lcd_draw_countdown = 0;
-const int lcd_draw_countdown_start = 128;
+const int lcd_draw_countdown_start = 5;
 
 // Function declarations *******************************************************
 void sendSignal(const int32_t & value);
@@ -92,10 +95,19 @@ void setup() {
 
     // Radio setup
     radio.begin();
-    radio.setPALevel(RF24_PA_HIGH);
+    Serial.println(F("Began the radio"));
+    radio.setPALevel(RF24_PA_LOW);
+    Serial.println(F("Set the PA"));
+    radio.enableAckPayload();                         // We will be using the Ack Payload feature, so please enable it
+    Serial.println(F("Enabled Ack Payload"));
+    radio.enableDynamicPayloads();                    // Ack payloads are dynamic payloads
+    Serial.println(F("Enabled dynamic payloads"));
     radio.openWritingPipe(writeAdress);
+    Serial.println(F("Opened writing pipe"));
     radio.openReadingPipe(1, readAdress);
+    Serial.println(F("Opened reading pipe"));
     radio.startListening();
+    Serial.println(F("Started listening"));
     Serial.println(F("Finished radio setup"));
 
     // Pin setup
@@ -110,20 +122,27 @@ void setup() {
     // LCD setup
     lcd.begin(16,2);
     lcd.print("penor bol");
+
+    // Settinginterrupts
+    pinMode(RADIO_INTERRUPT_PIN, INPUT_PULLUP);
     delay(2000);
+    attachInterrupt(0, radioInterrupt, LOW);             // Attach interrupt handler to interrupt #0 (using pin 2) on BOTH the sender and receiver
 }
 
 void loop() {
-    delay(1);
-    composeAndSendMessage();
-    Serial.println("ENDOFFRAME");
+    Serial.println(F("Reading joystick"));
     joy.readPositions();
+    composeAndSendMessage();
 
     if (lcd_draw_countdown <= 0) {
         writeLCD();
         lcd_draw_countdown = lcd_draw_countdown_start;
     }
     lcd_draw_countdown--;
+
+    Serial.print(F("End of frame "));
+    Serial.println(lcd_draw_countdown);
+    delay(5);
 }
 
 void writeLCD () {
@@ -146,7 +165,7 @@ int32_t composeMessage () {
     int32_t message = 0;
 
     Serial.print(F("zero message: "));
-    Serial.print(message);
+    Serial.println(message);
 
     // Check button, and add result to message (0 false, 1 true)
     static bool button_down_old = false;
@@ -157,7 +176,7 @@ int32_t composeMessage () {
     button_down_old = button_down_new;
 
     Serial.print(F("buttoned message: "));
-    Serial.print(message);
+    Serial.println(message);
 
     // Check joysticks
     joy.readPositions();
@@ -186,7 +205,7 @@ int32_t composeMessage () {
     message |= left_wheel_input | right_wheel_input;
 
     Serial.print(F("full message: "));
-    Serial.print(message);
+    Serial.println(message);
 
     return message;
 }
@@ -196,33 +215,22 @@ void sendSignal(const int32_t & message_out) {
         Serial.print(F("Sending message: "));
         Serial.println(message_out, HEX);
     }
-    radio.stopListening();
-    bool failed = !radio.write( &message_out, sizeof(message_out) );
-    radio.startListening();
-    if (failed) {
-        Serial.println(F("Failed to send. Giving up..."));
-        return;
-    }
-    int32_t started_waiting_at = micros();
-    boolean timeout = false;
-    // Wait for response
-    while (!radio.available()) {
-        if (micros() - started_waiting_at > TIMEOUT_MICROSECENDS) {
-            timeout = true;
-            break;
-        }
-    }
-    if (timeout) {
-        Serial.println(F("Response timed out."));
-        return;
-    }
-    int32_t message_in;
-    radio.read(&message_in, sizeof(message_in));
-    if (message_in == message_out) {
-        if (message_out != 0) {
-            Serial.println(F("Command send and recieved successfully"));
-        }
-    } else {
-        Serial.println(F("Message/response mismatch. Something went wrong"));
+
+    radio.startWrite(&message_out, sizeof(int32_t), 0);
+}
+
+void radioInterrupt (void) {
+    bool tx, fail, rx; // tx: successful transmit. fail: failed transmit. rx: recieved message
+    radio.whatHappened(tx, fail, rx); // What happened?
+
+    if ( tx ) { Serial.println(F("Send:OK")); }
+
+    if ( fail ) { Serial.println(F("Send:Failed")); }
+
+    if ( rx || radio.available()) {
+        uint32_t ack_message;
+        radio.read(&ack_message, sizeof(ack_message));
+        Serial.print(F("Ack: "));
+        Serial.println(ack_message);
     }
 }
